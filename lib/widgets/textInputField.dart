33329a05d195ac8541e4951881ea7da20fff080a
Mohammed Asif <mohammedasifparambil@gmail.com>
2021-08-31 00:08:12 +0530

    with textfields in settings page

---
+++ lib/widgets/textInputField.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+
+class TextInputField extends StatelessWidget {
+  final TextInputType keyBoardType;
+  final Color enabledBorderColor;
+  final double enabledBorderWidth;
+  final Color focusedBorderColor;
+  final double focusedBorderWidth;
+  final Color disabledBorderColor;
+  final double disabledBorderWidth;
+  final String labelText;
+  final Color labelTextColor;
+  final double labelTextSize;
+  final String helperText;
+  final Color helperTextColor;
+
+  TextInputField({
+    required this.keyBoardType,
+    required this.enabledBorderColor,
+    required this.enabledBorderWidth,
+    required this.disabledBorderColor,
+    required this.disabledBorderWidth,
+    required this.focusedBorderColor,
+    required this.focusedBorderWidth,
+    required this.labelText,
+    required this.helperText,
+    required this.labelTextColor,
+    required this.labelTextSize,
+    required this.helperTextColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      keyboardType: keyBoardType,
+      enabled: true,
+      style: TextStyle(color: Colors.white),
+      decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: enabledBorderColor,
+                width: enabledBorderWidth,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(22))),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: focusedBorderColor,
+              width: focusedBorderWidth,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(25)),
+          ),
+          disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: disabledBorderColor,
+                width: disabledBorderWidth,
+              ),
+              borderRadius: BorderRadius.all(Radius.circular(22))),
+          labelText: labelText,
+          labelStyle: TextStyle(
+            color: labelTextColor,
+            fontSize: labelTextSize,
+            fontWeight: FontWeight.bold,
+          ),
+          helperText: helperText,
+          helperStyle: TextStyle(color: helperTextColor)),
+    );
+  }
+}
