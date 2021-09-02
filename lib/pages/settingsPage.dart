c8d1be03d9e4043e36942fc818aeaee0357a3ab5
Mohammed Asif <mohammedasifparambil@gmail.com>
2021-09-02 15:26:07 +0530

    test teat

--- lib/pages/settingsPage.dart
+++ lib/pages/settingsPage.dart
@@ -5,13 +5,16 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:bulkmailer/mailer.dart';
 
-String senderEmail = new SettingPages() as String;
+String senderEmail =new SettingPages() as String;
+
 
 class SettingPages extends StatelessWidget {
+
   TextEditingController emailValue = TextEditingController();
   TextEditingController passwordValue = TextEditingController();
-  String? senderEmail;
+  String? senderEmail ;
   String? senderPassword;
+
 
   @override
   Widget build(BuildContext context) {
@@ -32,89 +35,105 @@ class SettingPages extends StatelessWidget {
       ),
       body: Center(
           child: ListView(
-        children: [
-          Center(
-            child: Container(
-                height: 150,
-                child: Center(
-                  child: Text(
-                    "Login to your account",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
-                  ),
-                )),
-          ),
-          Container(
-            child: TextInputField(
-              controller: emailValue,
-              disabledBorderColor: Colors.green,
-              disabledBorderWidth: 1,
-              enabledBorderColor: Colors.grey,
-              enabledBorderWidth: 0.5,
-              focusedBorderColor: Colors.white,
-              focusedBorderWidth: 1.5,
-              labelText: "Email",
-              labelTextColor: Colors.grey,
-              labelTextSize: 18,
-              keyBoardType: TextInputType.emailAddress,
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          Container(
-            child: TextInputField(
-              controller: passwordValue,
-              disabledBorderColor: Colors.green,
-              disabledBorderWidth: 1,
-              enabledBorderColor: Colors.grey,
-              enabledBorderWidth: 0.5,
-              focusedBorderColor: Colors.white,
-              focusedBorderWidth: 1.5,
-              labelText: "Password",
-              labelTextColor: Colors.grey,
-              labelTextSize: 18,
-              keyBoardType: TextInputType.visiblePassword,
-            ),
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          Container(
-            child: CustomButton(
-              onPressed: () {
-                senderEmail = emailValue.text;
-                senderPassword = passwordValue.text;
+            children: [
+              Center(
+                child: Container(
+                    height: 150,
+                    child: Center(
+                      child: Text(
+                        "Login to your account",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    )),
+              ),
+              Container(
+                child: TextInputField(
+                  controller: emailValue,
+                  disabledBorderColor: Colors.green,
+                  disabledBorderWidth: 1,
+                  enabledBorderColor: Colors.grey,
+                  enabledBorderWidth: 0.5,
+                  focusedBorderColor: Colors.white,
+                  focusedBorderWidth: 1.5,
+                  labelText: "Email",
+                  labelTextColor: Colors.grey,
+                  labelTextSize: 18,
+                  keyBoardType: TextInputType.emailAddress,
+                ),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                child: TextInputField(
+                  controller: passwordValue,
+                  disabledBorderColor: Colors.green,
+                  disabledBorderWidth: 1,
+                  enabledBorderColor: Colors.grey,
+                  enabledBorderWidth: 0.5,
+                  focusedBorderColor: Colors.white,
+                  focusedBorderWidth: 1.5,
+                  labelText: "Password",
+                  labelTextColor: Colors.grey,
+                  labelTextSize: 18,
+                  keyBoardType: TextInputType.visiblePassword,
+                ),
+              ),
 
-                //for routing to homepage on pressing login
-                if (senderEmail!.isNotEmpty && senderPassword!.isNotEmpty) {
-                  sendMail(
-                      senderEmail!, senderPassword!, 'iambasith123@gmail.com');
+              SizedBox(height: 20,),
 
-                  final snackBar = SnackBar(
-                    content: const Text('Yay! Successfully logged in'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                } else {
-                  final snackBar = SnackBar(
-                    content: const Text('Hey! Complete your credentials'),
-                  );
-                  ScaffoldMessenger.of(context).showSnackBar(snackBar);
-                }
-              },
-              buttonColor: Colors.blue,
-              buttonTextColor: Colors.white,
-              buttonText: "Login",
-              fontSize: 20,
-              padding: EdgeInsets.all(0),
-            ),
-          )
-        ],
-      )),
+              Container(
+                child: CustomButton(
+                  onPressed: () {
+                    senderEmail = emailValue.text;
+                    senderPassword = passwordValue.text;
+
+
+
+                    //for routing to homepage on pressing login
+                    if (senderEmail!.isNotEmpty && senderPassword!.isNotEmpty) {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => HomePage()),
+                      );
+
+                      //snackbar showing while succesful login
+                      final snackBar = SnackBar(
+                        content: const Text('Yay! Successfully logged in'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+
+                    }
+
+
+                    else {
+                      //snackbar while any of field is not filled
+
+
+                      final snackBar = SnackBar(
+                        content: const Text('Hey! Complete your credentials'),
+                      );
+                      ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                    }
+                  },
+
+                  buttonColor: Colors.blue,
+                  buttonTextColor: Colors.white,
+                  buttonText: "Login",
+                  fontSize: 20,
+                  padding: EdgeInsets.all(0),
+                ),
+
+              )
+            ],
+          )),
     );
   }
 }
+
